1d66e8828b0527ce00d07cc7fd5fb0405251c601
Madoc90 <115938201+Madoc90@users.noreply.github.com>
2026-06-04 14:10:51 -0700

    Initial commit

---
+++ src/main/java/net/dev/boss/datagen/ModModelProvider.java
@@ -0,0 +1,22 @@
+package net.dev.boss.datagen;
+
+import net.dev.boss.BossMod;
+import net.dev.boss.item.ModItems;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.ModelProvider;
+import net.minecraft.client.data.models.model.ModelTemplates;
+import net.minecraft.data.PackOutput;
+
+public class ModModelProvider extends ModelProvider {
+    public ModModelProvider(PackOutput output) {
+        super(output, BossMod.MOD_ID);
+    }
+
+    @Override
+    protected void registerModels(BlockModelGenerators blockModels, ItemModelGenerators itemModels) {
+        itemModels.generateFlatItem(ModItems.CURSED_INGOT.get(), ModelTemplates.FLAT_ITEM);
+
+
+    }
+}
